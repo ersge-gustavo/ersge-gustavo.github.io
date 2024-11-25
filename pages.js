bb702ba0597841389844acc70f5f93b3e38440fd
Gustavo <gustavo@economatica.com.br>
2024-11-25 10:43:24 +0000

    push.bat

--- pages.js
+++ pages.js
@@ -1,82 +1,82 @@
 
 
 function getPage ( numPage ) {
-switch (numPage) {
 
-case 0 : return [
-	'Menu',
-	'https://docs.google.com/document/d/e/2PACX-1vT4hN3Jq8Am_LXWY4-bqWP_Ox6132xIhDCZH9lOMqqO7hQ_-jCkVhCH1SqnnLvIUspv_IXmMXK8TJ86/pub'
-];
+	test = 0;
 
-case 1 : return [
-	'Fonctions en Javascript',
-	'https://docs.google.com/document/d/e/2PACX-1vRL03NXN4jrD84ycza_9ecrmWQk0JNasiwFguyzu2hxqMsi6_LHU05puzlXRg4ue-MSty_yrrl7fr-4/pub'
-];
 
-case 2 : return [
-	'Le jeu Papier / Caillou / Ciseaux',
-	'https://docs.google.com/document/d/e/2PACX-1vTR_ci7SiESvdVpm94wO6EXEoUYj6ta7_SRqcF5DMgIB7hvMy9HN_k0su99pL2t7GVHO3xvk6Q25ZRH/pub'
-];
+	if (numPage == test++ ) return [
+		'Introduction',
+		'introduction.html'
+	];
 
-case 3 : return [
-	'Choix de l’ordinateur, choix fixe au départ',
-	'https://docs.google.com/document/d/e/2PACX-1vQC4IaqAgYW-CD5jbXPR5F-02ckhjqIgwmuHEe0-LpkD3JzJXa_XHbat1PRp6__CeNxIZnAX9Q59QuS/pub'
-];
+	if (numPage == test++ ) return [
+		'Fonctions en Javascript',
+		'https://docs.google.com/document/d/e/2PACX-1vRL03NXN4jrD84ycza_9ecrmWQk0JNasiwFguyzu2hxqMsi6_LHU05puzlXRg4ue-MSty_yrrl7fr-4/pub'
+	];
 
-case 4 : return [
-	'Fonction pour le choix de l’ordinateur',
-	'https://docs.google.com/document/d/e/2PACX-1vRIhu9j2qdN6GsI89BO5lJhn3w4jPy40NnDEOU_8h_xEvETKar5pytfX-Y0RZe809M_g44mMlvvCdbR/pub'
-];
+	if (numPage == test++ ) return [
+		'Le jeu Papier / Caillou / Ciseaux',
+		'https://docs.google.com/document/d/e/2PACX-1vTR_ci7SiESvdVpm94wO6EXEoUYj6ta7_SRqcF5DMgIB7hvMy9HN_k0su99pL2t7GVHO3xvk6Q25ZRH/pub'
+	];
 
-case 5 : return [
-	'Modifier la fonction choixAleatoire()',
-	'https://docs.google.com/document/d/e/2PACX-1vRXQHucQvIZdtGIkeUIw3WZw7dHLv8mXJJnd95TO5svQ7WpKjr3qFj2zcIflvJO7MNoZAQE-gtnYZMV/pub'
-];
+	if (numPage == test++ ) return [
+		'Choix de l’ordinateur, choix fixe au départ',
+		'https://docs.google.com/document/d/e/2PACX-1vQC4IaqAgYW-CD5jbXPR5F-02ckhjqIgwmuHEe0-LpkD3JzJXa_XHbat1PRp6__CeNxIZnAX9Q59QuS/pub'
+	];
 
-case 6 : return [
-	'Distinguer entre caillou et ciseaux',
-	'https://docs.google.com/document/d/e/2PACX-1vRCxbE1-pG6zepfECXCUZO2_H6AykPZf0XCYvLMswH9wsuWkjOeS2lDTt5ILbLQpNk8CswzV1IXrdq8/pub'
-];
+	if (numPage == test++ ) return [
+		'Fonction pour le choix de l’ordinateur',
+		'https://docs.google.com/document/d/e/2PACX-1vRIhu9j2qdN6GsI89BO5lJhn3w4jPy40NnDEOU_8h_xEvETKar5pytfX-Y0RZe809M_g44mMlvvCdbR/pub'
+	];
 
-case 7 : return [
-	'Afficher le résultat: qui a gagné',
-	'https://docs.google.com/document/d/e/2PACX-1vTR5nOU7WlhU3xbQ89SWqCCRs6-8fzM_bQ1ZYP37z8GqGmXJz4khstuispVRZwTrfOzMtOEbncfJr6s/pub'
-];
+	if (numPage == test++ ) return [
+		'Modifier la fonction choixAleatoire()',
+		'https://docs.google.com/document/d/e/2PACX-1vRXQHucQvIZdtGIkeUIw3WZw7dHLv8mXJJnd95TO5svQ7WpKjr3qFj2zcIflvJO7MNoZAQE-gtnYZMV/pub'
+	];
 
-case 8 : return [
-	'Afficher notre choix comme image au lieu de texte',
-	'https://docs.google.com/document/d/e/2PACX-1vTMawkoZ-POhin4qPSV2wLQ5V04nFX7-K_ELBt-Ejn0vYYaVlZtm7JG9ydUCs5jw8l_huIgq311zanf/pub'
-];
+	if (numPage == test++ ) return [
+		'Distinguer entre caillou et ciseaux',
+		'https://docs.google.com/document/d/e/2PACX-1vRCxbE1-pG6zepfECXCUZO2_H6AykPZf0XCYvLMswH9wsuWkjOeS2lDTt5ILbLQpNk8CswzV1IXrdq8/pub'
+	];
 
-case 9 : return [
-	'Montrer le choix de l’ordinateur aussi comme image',
-	'https://docs.google.com/document/d/e/2PACX-1vR-fzAJdNTON6mO4mjQbzF8k6HL6RXuL1nKQYUaHWbBtlRkZlHHGFxZvMn0qBb0QflBDK1WtfpEqJOQ/pub'
-];
+	if (numPage == test++ ) return [
+		'Afficher le résultat: qui a gagné',
+		'https://docs.google.com/document/d/e/2PACX-1vTR5nOU7WlhU3xbQ89SWqCCRs6-8fzM_bQ1ZYP37z8GqGmXJz4khstuispVRZwTrfOzMtOEbncfJr6s/pub'
+	];
 
-case 10 : return [
-	'Changer l’affichage des images',
-	'https://docs.google.com/document/d/e/2PACX-1vTkwXxkoFqrPPBnv_C3JNQU1EeDCEmA_SwyHBxTHDTmKybIy11PKilEFYW-k-d3QK52iss7Q-EWl3Us/pub'
-];
+	if (numPage == test++ ) return [
+		'Afficher notre choix comme image au lieu de texte',
+		'https://docs.google.com/document/d/e/2PACX-1vTMawkoZ-POhin4qPSV2wLQ5V04nFX7-K_ELBt-Ejn0vYYaVlZtm7JG9ydUCs5jw8l_huIgq311zanf/pub'
+	];
 
-case 11 : return [
-	'Garder un score des résultats',
-	'https://docs.google.com/document/d/e/2PACX-1vT4ULwj9tkGqOz2m9wLHhyjJ-4mKP1i3UQQEKoUJs1fwW1sYUnUAzgjJcaFbc_Aqac7XaqvZJKhTt25/pub'
-];
+	if (numPage == test++ ) return [
+		'Montrer le choix de l’ordinateur aussi comme image',
+		'https://docs.google.com/document/d/e/2PACX-1vR-fzAJdNTON6mO4mjQbzF8k6HL6RXuL1nKQYUaHWbBtlRkZlHHGFxZvMn0qBb0QflBDK1WtfpEqJOQ/pub'
+	];
 
-case 12 : return [
-	'Une carte “Google Maps” dans notre page web',
-	'https://docs.google.com/document/d/e/2PACX-1vQ3Xg5kd082kdZwRDAusClR8A_Xy7Oailp1I2BEwcsmZ5vcETQ9e9X9WztuCVKiPowJuim-ohx4Rmjh/pub'
-];
+	if (numPage == test++ ) return [
+		'Changer l’affichage des images',
+		'https://docs.google.com/document/d/e/2PACX-1vTkwXxkoFqrPPBnv_C3JNQU1EeDCEmA_SwyHBxTHDTmKybIy11PKilEFYW-k-d3QK52iss7Q-EWl3Us/pub'
+	];
 
-case 13 : return [
-	'-- rien --',
-	'https://docs.google.com/document/d/e/2PACX-1vRL03NXN4jrD84ycza_9ecrmWQk0JNasiwFguyzu2hxqMsi6_LHU05puzlXRg4ue-MSty_yrrl7fr-4/pub'
-];
+	if (numPage == test++ ) return [
+		'Garder un score des résultats',
+		'https://docs.google.com/document/d/e/2PACX-1vT4ULwj9tkGqOz2m9wLHhyjJ-4mKP1i3UQQEKoUJs1fwW1sYUnUAzgjJcaFbc_Aqac7XaqvZJKhTt25/pub'
+	];
 
-default : return null;
+	if (numPage == test++ ) return [
+		'Une carte “Google Maps” dans notre page web',
+		'https://docs.google.com/document/d/e/2PACX-1vQ3Xg5kd082kdZwRDAusClR8A_Xy7Oailp1I2BEwcsmZ5vcETQ9e9X9WztuCVKiPowJuim-ohx4Rmjh/pub'
+	];
 
-}
-return null;
+	if (numPage == test++ ) return [
+		'-- rien --',
+		'https://docs.google.com/document/d/e/2PACX-1vRL03NXN4jrD84ycza_9ecrmWQk0JNasiwFguyzu2hxqMsi6_LHU05puzlXRg4ue-MSty_yrrl7fr-4/pub'
+	];
+
+
+	return null;
 }
 
 
