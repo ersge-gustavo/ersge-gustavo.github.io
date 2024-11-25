4890ef9bafcc04098fa3e7d3a09df64aaa71fcd8
Gustavo <gustavo@economatica.com.br>
2024-11-25 09:28:47 +0000

    push.bat

--- pages.js
+++ pages.js
@@ -9,67 +9,67 @@ case 0 : return [
 ];
 
 case 1 : return [
-	'1',
+	'Fonctions en Javascript',
 	'https://docs.google.com/document/d/e/2PACX-1vRL03NXN4jrD84ycza_9ecrmWQk0JNasiwFguyzu2hxqMsi6_LHU05puzlXRg4ue-MSty_yrrl7fr-4/pub'
 ];
 
 case 2 : return [
-	'2',
+	'Le jeu Papier / Caillou / Ciseaux',
 	'https://docs.google.com/document/d/e/2PACX-1vTR_ci7SiESvdVpm94wO6EXEoUYj6ta7_SRqcF5DMgIB7hvMy9HN_k0su99pL2t7GVHO3xvk6Q25ZRH/pub'
 ];
 
 case 3 : return [
-	'3',
+	'Choix de l’ordinateur, choix fixe au départ',
 	'https://docs.google.com/document/d/e/2PACX-1vQC4IaqAgYW-CD5jbXPR5F-02ckhjqIgwmuHEe0-LpkD3JzJXa_XHbat1PRp6__CeNxIZnAX9Q59QuS/pub'
 ];
 
 case 4 : return [
-	'4',
+	'Fonction pour le choix de l’ordinateur',
 	'https://docs.google.com/document/d/e/2PACX-1vRIhu9j2qdN6GsI89BO5lJhn3w4jPy40NnDEOU_8h_xEvETKar5pytfX-Y0RZe809M_g44mMlvvCdbR/pub'
 ];
 
 case 5 : return [
-	'5',
+	'Modifier la fonction choixAleatoire()',
 	'https://docs.google.com/document/d/e/2PACX-1vRXQHucQvIZdtGIkeUIw3WZw7dHLv8mXJJnd95TO5svQ7WpKjr3qFj2zcIflvJO7MNoZAQE-gtnYZMV/pub'
 ];
 
 case 6 : return [
-	'6',
+	'Distinguer entre caillou et ciseaux',
 	'https://docs.google.com/document/d/e/2PACX-1vRCxbE1-pG6zepfECXCUZO2_H6AykPZf0XCYvLMswH9wsuWkjOeS2lDTt5ILbLQpNk8CswzV1IXrdq8/pub'
 ];
 
 case 7 : return [
-	'7',
+	'Afficher le résultat: qui a gagné',
 	'https://docs.google.com/document/d/e/2PACX-1vTR5nOU7WlhU3xbQ89SWqCCRs6-8fzM_bQ1ZYP37z8GqGmXJz4khstuispVRZwTrfOzMtOEbncfJr6s/pub'
 ];
 
 case 8 : return [
-	'8',
+	'Afficher notre choix comme image au lieu de texte',
 	'https://docs.google.com/document/d/e/2PACX-1vTMawkoZ-POhin4qPSV2wLQ5V04nFX7-K_ELBt-Ejn0vYYaVlZtm7JG9ydUCs5jw8l_huIgq311zanf/pub'
 ];
 
 case 9 : return [
-	'9',
+	'Montrer le choix de l’ordinateur aussi comme image',
 	'https://docs.google.com/document/d/e/2PACX-1vR-fzAJdNTON6mO4mjQbzF8k6HL6RXuL1nKQYUaHWbBtlRkZlHHGFxZvMn0qBb0QflBDK1WtfpEqJOQ/pub'
 ];
 
 case 10 : return [
-	'10',
+	'Changer l’affichage des images',
 	'https://docs.google.com/document/d/e/2PACX-1vTkwXxkoFqrPPBnv_C3JNQU1EeDCEmA_SwyHBxTHDTmKybIy11PKilEFYW-k-d3QK52iss7Q-EWl3Us/pub'
 ];
 
 case 11 : return [
-	'11',
+	'Garder un score des résultats',
 	'https://docs.google.com/document/d/e/2PACX-1vT4ULwj9tkGqOz2m9wLHhyjJ-4mKP1i3UQQEKoUJs1fwW1sYUnUAzgjJcaFbc_Aqac7XaqvZJKhTt25/pub'
 ];
 
 case 12 : return [
-	'12',
+	'Une carte “Google Maps” dans notre page web',
 	'https://docs.google.com/document/d/e/2PACX-1vQ3Xg5kd082kdZwRDAusClR8A_Xy7Oailp1I2BEwcsmZ5vcETQ9e9X9WztuCVKiPowJuim-ohx4Rmjh/pub'
 ];
 
 case 13 : return [
-	'13',
+	'-- rien --',
 	'https://docs.google.com/document/d/e/2PACX-1vRL03NXN4jrD84ycza_9ecrmWQk0JNasiwFguyzu2hxqMsi6_LHU05puzlXRg4ue-MSty_yrrl7fr-4/pub'
 ];
 
