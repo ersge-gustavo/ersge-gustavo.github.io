b892096ab1afb69ea3b52b5ec4178539e150a0ce
Gustavo <gustavo@economatica.com.br>
2024-11-27 12:48:50 +0000

    push.bat

---
+++ gutil.js
@@ -0,0 +1,49 @@
+// 2024-11-26 by gustavo.exel@ersge.ch guexel@gmail.com
+
+
+function getParamFromURL ( paramName ) {
+	const urlParams = new URLSearchParams(window.location.search);
+	return urlParams.get('paramName') || 0;
+}
+
+
+
+function getCookie(cookieName) {
+    let cookies = document.cookie;
+    let cookieArray = cookies.split("; ");
+
+    for (let i = 0; i < cookieArray.length; i++) {
+        let cookie = cookieArray[i];
+        let [name, value] = cookie.split("=");
+   
+        if (name === cookieName) {
+          return decodeURIComponent(value);
+        }
+    }
+ 
+    return null;
+}
+
+
+function randomString ( length ) {
+	let st = '';
+	const chars = 'QWERTYUIOPASDFGHJKLZXCVBNM1234567890';
+	const charslen = chars.length;
+	for ( i=0; i<length; i++ ) {
+		st += chars.charAt(Math.floor(Math.random()*charslen));
+	}
+	return st;
+}
+
+
+function getIdBrowser() {
+	let idb = getCookie('idb');
+	if (idb == null) {
+		idb = randomString ( 8 );
+		console.log ( 'created idBrowser ',idb );
+		document.cookie = "idb="+idb+"; path=/";
+	}
+	return idb;
+}
+
+
--- pages.js
+++ pages.js
@@ -1,77 +1,93 @@
+// 2024-11-26 by gustavo.exel@ersge.ch guexel@gmail.com
 
 
 function getPage ( numPage ) {
 
+	// return fields are:
+	// [0] : title
+	// [1] : code for cookie
+	// [2] : needs unblocking true/false ?
+	// [3] : URL, only used after it's unblocked
+
 	test = 0;
 
-
 	if (numPage == test++ ) return [
-		'Introduction', 'Intro',
+		'Introduction', 'Intro', false,
 		'https://docs.google.com/document/d/e/2PACX-1vRQbazzegdrSO96qtf-ZjddeuoEqU4BqAwnRT-MRj_SRtB4aO-i7xMoUWQ9Z8ua7uY3oi8Tqr2DJxk8/pub'
 	];
 
 	if (numPage == test++ ) return [
-		'Fonctions en Javascript', 'FJSCR',
+		'Actions à partir de click de buttons', 'BtnOnClick', true,
+		'https://docs.google.com/document/d/e/2PACX-1vQKoeCxZt53m_HVj5pgnOHHu4Jq15ZBfVdHcnmIIF0q0W5CmUwfIiCcA-dJb1rd088Ka_vgNu0NLkBN/pub'
+	];
+
+
+
+	return null;
+
+
+	if (numPage == test++ ) return [
+		'Fonctions en Javascript', 'FuntionsJS', true,
 		'https://docs.google.com/document/d/e/2PACX-1vRL03NXN4jrD84ycza_9ecrmWQk0JNasiwFguyzu2hxqMsi6_LHU05puzlXRg4ue-MSty_yrrl7fr-4/pub'
 	];
 
 	if (numPage == test++ ) return [
-		'Le jeu Papier / Caillou / Ciseaux', 'JeuPCC',
+		'Le jeu Papier / Caillou / Ciseaux', 'JeuPCC', true,
 		'https://docs.google.com/document/d/e/2PACX-1vTR_ci7SiESvdVpm94wO6EXEoUYj6ta7_SRqcF5DMgIB7hvMy9HN_k0su99pL2t7GVHO3xvk6Q25ZRH/pub'
 	];
 
 	if (numPage == test++ ) return [
-		'Choix de l’ordinateur, choix fixe au départ', 'CDLOFAD',
+		'Choix de l’ordinateur, choix fixe au départ', 'ChoixOrdinateur', true,
 		'https://docs.google.com/document/d/e/2PACX-1vQC4IaqAgYW-CD5jbXPR5F-02ckhjqIgwmuHEe0-LpkD3JzJXa_XHbat1PRp6__CeNxIZnAX9Q59QuS/pub'
 	];
 
 	if (numPage == test++ ) return [
-		'Fonction pour le choix de l’ordinateur', 'FuncOrdi',
+		'Fonction pour le choix de l’ordinateur', 'FuncChoixOrdi', true,
 		'https://docs.google.com/document/d/e/2PACX-1vRIhu9j2qdN6GsI89BO5lJhn3w4jPy40NnDEOU_8h_xEvETKar5pytfX-Y0RZe809M_g44mMlvvCdbR/pub'
 	];
 
 	if (numPage == test++ ) return [
-		'Modifier la fonction choixAleatoire()', 'NFCALe',
+		'Modifier la fonction choixAleatoire()', 'MofidFuncChoixAle', true,
 		'https://docs.google.com/document/d/e/2PACX-1vRXQHucQvIZdtGIkeUIw3WZw7dHLv8mXJJnd95TO5svQ7WpKjr3qFj2zcIflvJO7MNoZAQE-gtnYZMV/pub'
 	];
 
 	if (numPage == test++ ) return [
-		'Distinguer entre caillou et ciseaux', 'DECCCis',
+		'Distinguer entre caillou et ciseaux', 'DistinguerCC', true,
 		'https://docs.google.com/document/d/e/2PACX-1vRCxbE1-pG6zepfECXCUZO2_H6AykPZf0XCYvLMswH9wsuWkjOeS2lDTt5ILbLQpNk8CswzV1IXrdq8/pub'
 	];
 
 	if (numPage == test++ ) return [
-		'Afficher le résultat: qui a gagné', 'ALRQAG',
+		'Afficher le résultat: qui a gagné', 'QuiAGagne', true,
 		'https://docs.google.com/document/d/e/2PACX-1vTR5nOU7WlhU3xbQ89SWqCCRs6-8fzM_bQ1ZYP37z8GqGmXJz4khstuispVRZwTrfOzMtOEbncfJr6s/pub'
 	];
 
 	if (numPage == test++ ) return [
-		'Afficher notre choix comme image au lieu de texte', 'ANCImg',
+		'Afficher notre choix comme image au lieu de texte', 'ImgNotreChoix', true,
 		'https://docs.google.com/document/d/e/2PACX-1vTMawkoZ-POhin4qPSV2wLQ5V04nFX7-K_ELBt-Ejn0vYYaVlZtm7JG9ydUCs5jw8l_huIgq311zanf/pub'
 	];
 
 	if (numPage == test++ ) return [
-		'Montrer le choix de l’ordinateur aussi comme image', 'ImgCDLOrdi',
+		'Montrer le choix de l’ordinateur aussi comme image', 'ImgChoixOrdi', true,
 		'https://docs.google.com/document/d/e/2PACX-1vR-fzAJdNTON6mO4mjQbzF8k6HL6RXuL1nKQYUaHWbBtlRkZlHHGFxZvMn0qBb0QflBDK1WtfpEqJOQ/pub'
 	];
 
 	if (numPage == test++ ) return [
-		'Changer l’affichage des images', 'ImgChgAffichage',
+		'Changer l’affichage des images', 'ImgsFaceAFace', true,
 		'https://docs.google.com/document/d/e/2PACX-1vTkwXxkoFqrPPBnv_C3JNQU1EeDCEmA_SwyHBxTHDTmKybIy11PKilEFYW-k-d3QK52iss7Q-EWl3Us/pub'
 	];
 
 	if (numPage == test++ ) return [
-		'Garder un score des résultats', 'ScoreRes',
+		'Garder un score des résultats', 'ScoreResult', true,
 		'https://docs.google.com/document/d/e/2PACX-1vT4ULwj9tkGqOz2m9wLHhyjJ-4mKP1i3UQQEKoUJs1fwW1sYUnUAzgjJcaFbc_Aqac7XaqvZJKhTt25/pub'
 	];
 
 	if (numPage == test++ ) return [
-		'Une carte “Google Maps” dans notre page web', 'GMapsDNP',
+		'Une carte “Google Maps” dans notre page web', 'EmbedGMaps', true,
 		'https://docs.google.com/document/d/e/2PACX-1vQ3Xg5kd082kdZwRDAusClR8A_Xy7Oailp1I2BEwcsmZ5vcETQ9e9X9WztuCVKiPowJuim-ohx4Rmjh/pub'
 	];
 
 	if (numPage == test++ ) return [
-		'-- rien --', 'rien',
+		'-- rien --', 'rien', true,
 		'https://docs.google.com/document/d/e/2PACX-1vRL03NXN4jrD84ycza_9ecrmWQk0JNasiwFguyzu2hxqMsi6_LHU05puzlXRg4ue-MSty_yrrl7fr-4/pub'
 	];
 
